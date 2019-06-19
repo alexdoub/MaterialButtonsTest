5d517c15ec03fe13b6b6ac27713564f0783c654d
Alex Doub <alexdoub1990@gmail.com>
2019-06-18 20:19:00 -0700

    Added buttons and layed them out. added rx

--- app/src/main/java/alex/com/materialapptest/MainActivity.kt
+++ app/src/main/java/alex/com/materialapptest/MainActivity.kt
@@ -1,13 +1,41 @@
 package alex.com.materialapptest
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+        Observable.interval(2, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                toggleButtons()
+            }
+    }
+
+    fun toggleButtons() {
+        val root = findViewById<LinearLayout>(R.id.da_root)
+        for (i in 0..root.childCount) {
+            val child = root.getChildAt(i)
+            if (child is Button) {
+                child.isEnabled = !child.isEnabled
+            } else if (child is MaterialButton) {
+                child.isEnabled = !child.isEnabled
+            }
+        }
     }
 }
